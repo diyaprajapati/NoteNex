502aadbd210970cb9300088b9f61f2036e1c1af7
diyaprajapati <diyabprajapati2005@gmail.com>
2024-02-25 02:11:47 +0530

    update diary and todo

--- src/diary.js
+++ src/diary.js
@@ -2,3 +2,18 @@
 document.getElementById('dropdown').addEventListener('click', function() {
     this.classList.toggle('open');
   });
+
+//store in local storage
+document.addEventListener('DOMContentLoaded', function() {
+  const diaryText = localStorage.getItem('diaryText');
+  const textarea = document.getElementById('diary-text');
+  
+  if (diaryText) {
+      textarea.value = diaryText;
+  }
+
+  textarea.addEventListener('input', function() {
+      const updatedDiaryText = textarea.value;
+      localStorage.setItem('diaryText', updatedDiaryText);
+  });
+});
