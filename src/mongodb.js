57f1010f28ccffa61785102d4f0b5ce652d2bf0b
diyaprajapati <diyabprajapati2005@gmail.com>
2024-02-25 22:15:42 +0530

    update vercel config

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect('mongodb://localhost:27017/LoginSignup')
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('mongodb connected');
 })
